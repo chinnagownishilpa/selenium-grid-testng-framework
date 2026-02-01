cfb047730131c60f09ca8917068459dc8d54998c
Shilpa <chinnagownishilpa@gmail.com>
2026-02-01 09:12:08 +0530

    modified runner class to run test in parellel

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Guru99Login.feature",
+        features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
